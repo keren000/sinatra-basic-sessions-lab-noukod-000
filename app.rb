92cad3b85c8af62088d9c5cfc3264770a7ddea32
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-11 21:03:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @sessions = session
-    item = params["item"]
-    @sessions[:item] = item
+    content = params["item"]
+    @sessions[:content] = item
   end
 end
